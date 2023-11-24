b5ff5255bd2d965fd3e62f0c8ffc0c2fa551304a
Joshua Willson <joshuawillsonmj@gmail.com>
2023-11-24 20:42:55 +0200

    added favicon + worked on header and preview

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
-export default function Preview() {
+export default function Header() {
 
     return (
-        <h1 className="header--heading">The Podcast App</h1>
+        <div className="header--container">
+            <img className="header--img" src="../src/images/microphone.png" alt="Microphone Image" />
+            <h1 className="header--heading">The Podcast App</h1>
+        </div>
     )
 }
 
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,27 +1,55 @@
 import { useState, useEffect } from "react";
 import { genres } from "../genre-data";
 
-export default function Preview() {
-    const [previewData, setPreviewData] = useState([])
+export default function Preview(props) {
+    const {} = props
+    const [previewData, setPreviewData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [endSliceValue, setEndSliceValue] = useState(10);
+    const isDisabled = previewData.slice(0, endSliceValue).length === previewData.length
 
     useEffect(() => {
+        setIsLoading(true)
         fetch("https://podcast-api.netlify.app/shows")
         .then(response => response.json())
-        .then(data => setPreviewData(data))
+        .then(data => {
+            setPreviewData(data)
+            setIsLoading(false)
+        })
     }, [])
 
     const displayPreviewData = () => [
-        previewData.map(({title, image, id, description, seasons, genres, updated}) => {
+        previewData.slice(0, endSliceValue).map(({title, image, id, description, seasons, genres, updated}) => {
             return <div key={id} className="preview--item">
                         <h3>{title}</h3>
-                        <img src={image} alt="Preview Image" width="100" />
+                        <div className="preview--img--desc">
+                            <img className="preview--img" src={image} alt="Preview Image" />
+                            <h4 className="preview--desc">{description}</h4>
+                        </div>
                    </div>
         })
     ]
 
+    const clickHandler = () => {
+        setEndSliceValue(prev => prev + prev)
+    }
+
+    const calcRemainingItems = () => {
+        return previewData.slice(0, endSliceValue).length === previewData.length 
+        ? 0 
+        : previewData.length - endSliceValue
+    }
+
     return (
         <div className="preview--container">
-           {displayPreviewData()}
+           {isLoading ? <h2 className="preview--loading">Loading...</h2> 
+           : <div>
+                {displayPreviewData()} 
+                <button disabled={isDisabled} 
+                        className="preview--more--button" 
+                        onClick={clickHandler}>Show more ({calcRemainingItems()})
+                </button>
+             </div>}
         </div>
     )
 }
